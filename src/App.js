74d95948d1cd6fef29d473890c7e73006f945a27
Nick Geofroy <nickgeofroy1998@gmail.com>
2020-12-04 13:54:18 -0500

    get auth working locally

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const queryString = require('query-string')
+class App extends React.Component {
+  render() {
+    console.log(window.location)
+    var queryParams = queryString.parse(window.location.hash)
+    console.log("query params: ", queryParams)
+    if ("access_token" in queryParams || window.location.toString().includes("access_token")) {
+      return (
+        <div className="App">Success</div>
+      )
+    } else {
+      var query = {
+        "client_id": "cad5fe116d264749a77638ec4bfb4a2f",
+        "response_type": "token",
+        "redirect_uri": process.env.REACT_APP_DOMAIN,
+        "scope": "playlist-modify-private",
+        "state": "logged"
+      }
+      console.log(query)
+      var base_url = "https://accounts.spotify.com/authorize?"
+      window.location.href = base_url + queryString.stringify(query)
+      console.log("public url: " + process.env.REACT_APP_DOMAIN)
+      return (
+        <div className="App">
+          {process.env.PUBLIC_URL}
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
